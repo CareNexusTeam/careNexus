9eaf5fcbcb6d4225da977ca29195450a10982c92
Anurag07 <anurag.kushwaha@cognizant.com>
2026-07-09 17:00:13 +0530

    delete useless code

--- carenexus/src/main/java/com/cts/carenexus/patientManagement/entities/MedicalStatus.java
+++
@@ -1,6 +0,0 @@
-package com.cts.carenexus.patientManagement.entities;
-
-public enum MedicalStatus {
-    Active,
-    Resolved
-}
--- carenexus/src/main/java/com/cts/carenexus/patientManagement/entities/PatientStatus.java
+++
@@ -1,6 +0,0 @@
-package com.cts.carenexus.patientManagement.entities;
-
-public enum PatientStatus {
-    Active,
-    Inactive,
-}

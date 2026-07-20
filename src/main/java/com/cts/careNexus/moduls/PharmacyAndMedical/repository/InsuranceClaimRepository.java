0d5769d6afb358bbd3b8befbcbb128379b5aaa04
Singh <2505436@cognizant.com>
2026-07-20 08:47:05 +0530

    module

--- src/main/java/com/cts/careNexus/moduls/PharmacyAndMedical/repository/InsuranceClaimRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cts.careNexus.moduls.billingandinsurance.PharmacyAndMedical.repository;
-
-import com.cts.careNexus.moduls.billingandinsurance.PharmacyAndMedical.model.InsuranceClaim;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InsuranceClaimRepository extends JpaRepository<InsuranceClaim,Long> {
-
-}
--- src/main/java/com/cts/careNexus/moduls/PharmacyAndMedical/repository/InvoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cts.careNexus.moduls.billingandinsurance.PharmacyAndMedical.repository;
-
-import com.cts.careNexus.moduls.billingandinsurance.PharmacyAndMedical.model.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
-}

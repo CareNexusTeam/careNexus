732987dca1303c2122433af459b8a1b9fcc9abfb
Racheet <Racheet.Saraswat@cognizant.com>
2026-07-22 10:51:54 +0530

    cleaning workspace for updated code

--- src/main/java/com/cts/careNexus/moduls/workflow_emr/repository/ConsultationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.careNexus.moduls.appointment_schedule.workflow_emr.repository;
-
-import com.cts.careNexus.moduls.appointment_schedule.workflow_emr.entity.Consultation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
-}
